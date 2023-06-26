204eb90ef697baa0edc5920235e043e6e6f21a19
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-06-26 12:30:56 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    int T;
+    
+    cin >> T;
+    
+    for(int i=0;i<T;i++)
+    {
+        cin >> str;
+        cout << str[0] << str[str.length()-1] << endl;
+    }
+    
+    return 0;
+}
