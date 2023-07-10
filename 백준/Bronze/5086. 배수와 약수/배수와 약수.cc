8e8c6987d76f9da71412282a1b8d7d3d0774a477
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-07-10 14:04:24 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    while(true)
+    {
+        cin >> a >> b;
+        if(a==0 && b==0)
+            break;
+        else if(a==0 || b==0)
+            cout << "neither" << endl;
+        else if(a>b && a%b==0)
+            cout << "multiple" << endl;
+        else if(a<b && b%a==0)
+            cout << "factor" << endl;
+        else
+            cout << "neither" << endl;
+    }
+    
+    return 0;
+}
