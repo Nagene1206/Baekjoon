a0e40954ce46ca074a72a4dc08ab3b123deb6ab6
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-07-10 14:12:48 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N,K;
+    
+    cin >> N >> K;
+    
+    for(int i=1;i<=N;i++)
+    {
+        if(N%i==0)
+        {
+            K--;
+        }
+        if(K==0)
+        {
+            cout << i;
+            break;
+        }
+    }
+    
+    if(K>0)
+        cout << 0; 
+    
+    return 0;
+}
