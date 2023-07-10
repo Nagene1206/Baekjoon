092bef0fbff9c5ed4c907b5575a426667f05a263
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-07-10 15:20:21 +0900

    [Bronze II] Title: 소수, Time: 160 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m,n;
+    int cnt=0,sum=0,min=-1;
+    
+    cin >> m >> n;
+    
+    for(int i=m;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+                cnt++;
+        }
+        
+        if(cnt==2)
+        {
+            sum+=i;
+            if(min==-1)
+            {
+                min = i;
+            }
+        }
+        cnt=0;
+    }
+    
+    if(min==-1)
+        cout << "-1";
+    else
+        cout << sum << endl << min;
+    return 0;
+}
