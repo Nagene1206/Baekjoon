2d7b9670a567ea208b934331d3521d9079b241bc
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-07-17 12:53:11 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int num[5];
+    int sum=0;
+    
+    for(int i=0;i<5;i++)
+    {
+        cin >> num[i];
+        sum+=num[i];
+    }
+
+    sort(num,num+5);
+    
+    cout << (float)sum/5 << "\n" << num[2];
+    
+    
+    return 0;
+}
