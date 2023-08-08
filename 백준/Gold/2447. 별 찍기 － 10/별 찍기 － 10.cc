a9d6000e6d7ad7eedf47da21045127b1dd9eb73f
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-08-08 15:00:17 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 296 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void draw_star(int i,int j,int n)
+{
+    if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
+        cout << " ";
+    else if (n / 3 == 0)
+        cout << "*";
+    else
+        draw_star(i, j, n / 3);
+}
+
+int main() 
+{
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            draw_star(i, j, n);
+        }
+        cout << "\n";
+    }
+}
