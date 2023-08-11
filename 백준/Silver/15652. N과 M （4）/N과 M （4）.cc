5334586724b33e8851d00512b9558cd24914e2ec
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2023-08-11 13:31:39 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9];
+bool visited[9];
+
+void btrack(int depth, int j)
+{
+    if (depth == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+
+        return;
+    }
+    for (j; j <= n; j++)
+    {
+        if (!visited[j])
+        {
+            arr[depth] = j;
+            btrack(depth + 1,j);
+        }
+    }
+}
+
+int main() 
+{
+    cin >> n >> m;
+    btrack(0,1);
+}
