28417963a920bfd390d95d8735115e04ce59567e
Myoungjun Jung <116701689+Nagene1206@users.noreply.github.com>
2024-10-26 16:15:04 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 12.78 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -7,14 +7,14 @@ using namespace std;
 long long solution(int r1, int r2) {
     long long answer = 0;
     
-    for(int i=1;i<r2;++i)
+    for(int i=1;i<r2;++i)   //x좌표
     {
         int max = 0;
         int min = 0;
         
-        max = (int)floor(sqrt(pow(r2,2)-pow(i,2)));
+        max = (int)floor(sqrt(pow(r2,2)-pow(i,2))); //y의 최댓값 , 원의 방정식
         
-        if(i<r1) min = (int)ceil(sqrt(pow(r1,2)-pow(i,2)));
+        if(i<r1) min = (int)ceil(sqrt(pow(r1,2)-pow(i,2))); //y의 최솟값
         else min = 1;
         
         answer += max - min + 1;
